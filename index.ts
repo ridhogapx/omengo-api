8e4ea0a651d8551bd61a962bea1cc4c1fde710de
RageNeko26 <rneko2006@gmail.com>
2023-06-06 22:47:50 +0700

    feat: Implement Agent controller in index

--- index.ts
+++ index.ts
@@ -1,8 +1,11 @@
 import express, { Express } from "express"
+import { allAgents } from "./controllers/Agent"
 
 const app: Express = express()
 
 const port: number = 4000
+
+app.get("/api/v1/agent", allAgents)
 
 app.listen(port, (): void => {
     console.log(`Server is running on port ${port}`)
