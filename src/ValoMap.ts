37d9e011657e517ef780f80cc6409c0a21122136
RageNeko26 <rneko2006@gmail.com>
2023-06-07 00:50:26 +0700

    feat: Adding Map data

---
+++ src/ValoMap.ts
@@ -0,0 +1,44 @@
+import { DataMap } from "../interfaces/ResponseAPI"
+
+const ValoMap: DataMap[] = [
+    {
+        id: 1,
+        name: "Ascent",
+        image_url: "https://static.wikia.nocookie.net/valorant/images/9/91/Ascent_transparent_bg.png/revision/latest/scale-to-width-down/200?cb=20210620033237"
+    },
+    {
+        id: 2,
+        name: "Bind",
+        image_url: "https://static.wikia.nocookie.net/valorant/images/e/e1/Bind_transparent_bg.png/revision/latest/scale-to-width-down/200?cb=20210620033339"
+    },{
+        id: 3,
+        name: "Breeze",
+        image_url: "https://static.wikia.nocookie.net/valorant/images/f/fc/Breeze_transparent_bg.png/revision/latest/scale-to-width-down/200?cb=20210620033352"
+    },{
+        id: 4,
+        name: "Fracture",
+        image_url: "https://static.wikia.nocookie.net/valorant/images/9/97/Fracture_transparent_bg.png/revision/latest/scale-to-width-down/200?cb=20210918001818"
+    },{
+        id: 5,
+        name: "Haven",
+        image_url: "https://static.wikia.nocookie.net/valorant/images/c/c0/Haven_transparent_bg.png/revision/latest/scale-to-width-down/200?cb=20210620033327"
+    },{
+        id: 6,
+        name: "Icebox",
+        image_url: "https://static.wikia.nocookie.net/valorant/images/e/ec/Icebox_transparent_bg.png/revision/latest/scale-to-width-down/200?cb=20210620033416"
+    },{
+        id: 7,
+        name: "Lotus",
+        image_url: "https://static.wikia.nocookie.net/valorant/images/3/37/Lotus_transparent_bg.png/revision/latest/scale-to-width-down/200?cb=20230110145006"
+    },{
+        id: 8,
+        name: "Pearl",
+        image_url: "https://static.wikia.nocookie.net/valorant/images/6/6f/Pearl_transparent_bg.png/revision/latest/scale-to-width-down/200?cb=20220622132830"
+    },{
+        id: 9,
+        name: "Split",
+        image_url: "https://static.wikia.nocookie.net/valorant/images/f/f0/Split_transparent_bg.png/revision/latest/scale-to-width-down/200?cb=20210620033313"
+    },
+]
+
+export default ValoMap
