96a9e4e5f8f9124236c5705e5fa1a7d3011bf35d
RageNeko26 <rneko2006@gmail.com>
2023-06-06 23:34:00 +0700

    fix: Single Agent for /api/v1/agent

--- controllers/Agent.ts
+++ controllers/Agent.ts
@@ -1,7 +1,18 @@
 import RouteCallback from "../types/RouteCallback"
-import Agents from "../src/Agents/Controller"
+import Agents from "../src/Agents/Controllers"
+import { DataAgent } from "../interfaces/ResponseAPI"
 
 export const allAgents: RouteCallback = (req, res) => {
     return res.json(Agents)    
 }
 
+export const singleAgent: RouteCallback = (req, res) => {
+    const { id } = req.params
+   
+    const singleAgent = Agents.filter((agent: DataAgent) => {
+        return Number(id) == agent.id
+    })
+
+    return res.json(singleAgent)
+
+}
--- index.ts
+++ index.ts
@@ -1,11 +1,12 @@
 import express, { Express } from "express"
-import { allAgents } from "./controllers/Agent"
+import { allAgents, singleAgent } from "./controllers/Agent"
 
 const app: Express = express()
 
 const port: number = 4000
 
 app.get("/api/v1/agent", allAgents)
+app.get("/api/v1/agent/:id", singleAgent)
 
 app.listen(port, (): void => {
     console.log(`Server is running on port ${port}`)
