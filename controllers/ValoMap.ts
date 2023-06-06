600b5a59035d17bb25180ea519522770d992b800
RageNeko26 <rneko2006@gmail.com>
2023-06-07 01:14:43 +0700

    feat: callback for single map

--- controllers/ValoMap.ts
+++ controllers/ValoMap.ts
@@ -1,13 +1,29 @@
 import ValoMap from "../src/ValoMap"
 import RouteCallback from "../types/RouteCallback"
-import { ResponseMap } from "../interfaces/ResponseAPI"
+import { ResponseMap, DataMap } from "../interfaces/ResponseAPI"
 
 export const allMap: RouteCallback = (req, res) => {
    const response: ResponseMap = {
         message: "Success fetching data maps!",
-        status: 200,
         success: true,
+        status: 200,
         data: ValoMap
+    }
+
+    return res.json(response)
+}
+
+export const singleMap: RouteCallback = (req, res) => {
+    const id: number = Number(req.params.id)
+    const single: DataMap[] = ValoMap.filter((result: DataMap) => {
+        return id == result.id
+    })
+
+    const response: ResponseMap = {
+        message: `Getting map id ${id} is success!`,
+        success: true,
+        status: 200,
+        data: single
     }
 
     return res.json(response)
