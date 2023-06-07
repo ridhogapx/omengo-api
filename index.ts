0322b0d3edc3dc536e9a35b6526364f838976497
RageNeko26 <rneko2006@gmail.com>
2023-06-07 08:02:19 +0700

    feat: Adding route /api/v1/map/:id for getting single map by id

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import express, { Express } from "express"
 import { allAgents, singleAgent } from "./controllers/Agent"
-import { allMap } from "./controllers/ValoMap"
+import { allMap, singleMap } from "./controllers/ValoMap"
 
 const app: Express = express()
 
@@ -9,6 +9,7 @@ const port: number = 4000
 app.get("/api/v1/agent", allAgents)
 app.get("/api/v1/agent/:id", singleAgent)
 app.get("/api/v1/map", allMap)
+app.get("/api/v1/map/:id", singleMap )
 
 app.listen(port, (): void => {
     console.log(`Server is running on port ${port}`)
