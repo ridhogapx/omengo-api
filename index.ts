0a7cc839ef09c81b1438746ff228551a1fd2809c
RageNeko26 <rneko2006@gmail.com>
2023-06-07 15:08:30 +0700

    feat: Adding cors

--- index.ts
+++ index.ts
@@ -2,7 +2,10 @@ import express, { Express } from "express"
 import { allAgents, singleAgent } from "./controllers/Agent"
 import { allMap, singleMap } from "./controllers/ValoMap"
 
+const cors = require('cors')
+
 const app: Express = express()
+app.use(cors())
 
 const port: number = 4000
 
